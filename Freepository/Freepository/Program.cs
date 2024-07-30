f0da870308cd3bf16bc2d3333ebe9c9adbb5af04
teclir <raimondotecla@gmail.com>
2024-07-30 09:49:40 +0200

    created CORS and ReadMe

--- Freepository/Freepository/Program.cs
+++ Freepository/Freepository/Program.cs
@@ -9,37 +9,49 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Microsoft.Extensions.Options;
-using Freepository.Models;
 using Freepository.Services;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configurar DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+// Configurar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder
+        .WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+
+builder.Services.AddControllers();
+
+// Configurar Identity
 builder.Services.AddIdentity<User, IdentityRole>(options =>
-    {
-        options.Password.RequireDigit = true;
-        options.Password.RequireLowercase = true;
-        options.Password.RequireUppercase = true;
-        options.Password.RequireNonAlphanumeric = true;
-        options.Password.RequiredLength = 6;
-        
-    }).AddEntityFrameworkStores<ApplicationDbContext>();
-    
-    builder.Services.AddAuthentication(options =>
-        {
-            options.DefaultAuthenticateScheme =
-                options.DefaultChallengeScheme =
-                    options.DefaultForbidScheme =
-                        options.DefaultScheme =
-                            options.DefaultSignInScheme =
-                                options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
-        }).AddJwtBearer(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 6;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Configurar autenticación JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultForbidScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
 {
     options.TokenValidationParameters = new TokenValidationParameters
     {
@@ -49,21 +61,30 @@ builder.Services.AddIdentity<User, IdentityRole>(options =>
         ValidAudience = builder.Configuration["JwtSettings:Audience"],
         ValidateIssuerSigningKey = true,
         IssuerSigningKey = new SymmetricSecurityKey(
-            System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SigningKey"]))
+            Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SigningKey"]))
     };
 });
 
+// Configurar repositorios
 builder.Services.AddScoped<IResourceRepository, ResourceRepository>();
 builder.Services.AddScoped<ITagRepository, TagRepository>();
 builder.Services.AddScoped<IRoadmapRepository, RoadmapRepository>();
 builder.Services.AddScoped<ICourseRepository, CourseRepository>();
 builder.Services.AddScoped<IBootcampRepository, BootcampRepository>();
+
+// Configurar AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
+
+// Configurar JSON y controladores
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
 {
     options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
 });
+
+// Configurar TokenService
 builder.Services.AddScoped<ITokenService, TokenService>();
+
+// Configurar Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(option =>
 {
@@ -84,11 +105,11 @@ builder.Services.AddSwaggerGen(option =>
             {
                 Reference = new OpenApiReference
                 {
-                    Type=ReferenceType.SecurityScheme,
-                    Id="Bearer"
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
                 }
             },
-            new string[]{}
+            new string[] {}
         }
     });
 });
@@ -102,7 +123,24 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("CorsPolicy");
+
 app.UseAuthentication();
 app.UseAuthorization();
+
+// Middleware para registrar solicitudes y respuestas
+app.Use(async (context, next) =>
+{
+    await next();
+
+    // Log de los encabezados de la respuesta
+    var corsHeaders = context.Response.Headers.Where(h => h.Key.StartsWith("Access-Control-"));
+    foreach (var header in corsHeaders)
+    {
+        Console.WriteLine($"{header.Key}: {header.Value}");
+    }
+});
+
 app.MapControllers();
 app.Run();
